f03101a23f292d9fba5d3e2ae56b1e6fad1cc89c
Marat Minchik <devminrat@gmail.com>
2023-05-09 21:53:03 +0300

    added intro animation

--- src/assets/js/components/animation.js
+++ src/assets/js/components/animation.js
@@ -9,8 +9,6 @@ if (priceFixBtn && window.innerWidth <= 500) {
       start: "top bottom-=200",
       end: "+=300 center",
       scrub: true,
-
-      markers: { startColor: "green", endColor: "red", fontSize: "12px" },
     },
   });
 
@@ -26,3 +24,143 @@ if (priceFixBtn && window.innerWidth <= 500) {
     "-=40%"
   );
 }
+const introBlock = document.querySelector(".intro");
+
+const introFruits = document.querySelectorAll(
+  ".intro-slider__first .anim-fruit"
+);
+const introAlmond = document.querySelector(".is-almond");
+const introPrunes = document.querySelector(".is-prunes");
+const introCherry = document.querySelector(".is-cherry");
+const introHollowCircle = document.querySelector(".hollow-circle");
+const introFilledCircle = document.querySelector(".filled-circle");
+const introImg = document.querySelector(".is-slide__main-img");
+
+const introTl = gsap.timeline();
+
+introTl
+  .fromTo(
+    introHollowCircle,
+    {
+      scale: 0,
+    },
+    {
+      scale: 1,
+      duration: 0.6,
+    }
+  )
+  .fromTo(
+    introFilledCircle,
+    {
+      scale: 0,
+    },
+    {
+      scale: 1,
+      duration: 0.6,
+    }
+  )
+  .fromTo(
+    introImg,
+    {
+      scale: 0,
+      opacity: 0,
+    },
+    {
+      scale: 1,
+      opacity: 1,
+      duration: 0.6,
+    }
+  )
+  .from(introAlmond, {
+    y: -300,
+    opacity: 0,
+  })
+  .from(
+    introPrunes,
+    {
+      y: -1000,
+      opacity: 0,
+    },
+    "-=0.2"
+  )
+  .from(
+    introCherry,
+    {
+      y: -800,
+      opacity: 0,
+    },
+    "-=0.2"
+  )
+  .to(introHollowCircle, {
+    duration: 4,
+    rotate: 360,
+    ease: "power3.out",
+  })
+  .to(introFruits, {
+    onComplete: () => {
+      console.log(this);
+      introFruits.forEach((el) => {
+        el.classList.add("active");
+      });
+    },
+  });
+
+const introSliderHeader = document.querySelector(".intro-slider__first-header");
+const introLeftLine = document.querySelector(".intro-slider .left-line");
+const introRightLine = document.querySelector(".intro-slider .right-line");
+
+gsap.fromTo(
+  introSliderHeader,
+  {
+    x: -500,
+    opacity: 0,
+  },
+  {
+    x: 0,
+    opacity: 1,
+    duration: 1,
+    ease: "power3.out",
+  },
+  "2"
+);
+gsap.to(introSliderHeader, {
+  scrollTrigger: {
+    trigger: introBlock,
+    start: "top top",
+    end: "bottom+=300 center",
+    scrub: true,
+  },
+  y: "100%",
+});
+gsap.to(introLeftLine, {
+  scrollTrigger: {
+    trigger: introBlock,
+    start: "top top",
+    end: "bottom-=200 center",
+    scrub: true,
+  },
+  opacity: 0,
+});
+
+gsap.fromTo(
+  introLeftLine,
+  {
+    width: "0rem",
+  },
+  {
+    width: "53rem",
+    duration: 1,
+  },
+  "1"
+);
+gsap.fromTo(
+  introRightLine,
+  {
+    width: "0rem",
+  },
+  {
+    width: "63.6rem",
+    duration: 1,
+  },
+  "1"
+);
